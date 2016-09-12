0fd2d1f3ec3673edde0bab57c0ebc6ae087a4357
Aidan Cornelius-Bell <aidan.cornelius-bell@flinders.edu.au>
2016-09-12 16:36:51 +0930

    Added message ID

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,7 @@
+require 'digest/sha2'
 class ApplicationMailer < ActionMailer::Base
   default from: "Bridging the Gap <no-reply@bridgingthegap.edu.au>",
-          reply_to: 'aidan.cornelius-bell@flinders.edu.au'
+          reply_to: 'aidan.cornelius-bell@flinders.edu.au',
+          "Message-ID" => "#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@bridgingthegap.edu.au"
   layout 'mailer'
 end
--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -1,7 +1,9 @@
+require 'digest/sha2'
 class CustomDeviseMailer < Devise::Mailer
     default from: "Bridging the Gap <no-reply@bridgingthegap.edu.au>",
             reply_to: 'aidan.cornelius-bell@flinders.edu.au',
-            template_path: 'devise/mailer'
+            template_path: 'devise/mailer',
+            "Message-ID" => "#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@bridgingthegap.edu.au"
 
     helper :application
     include Devise::Controllers::UrlHelpers
