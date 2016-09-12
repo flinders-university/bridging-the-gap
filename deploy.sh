37cd126dd0d84de3040509013e934d9be0d89046
Aidan Cornelius-Bell <aidan.cornelius-bell@flinders.edu.au>
2016-09-12 15:46:13 +0930

    Cleaned deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #/bin/bash
-/usr/local/rvm/bin/rvm use 2.3.1
-/usr/local/rvm/bin/rvm repair all
+if [[ -s "$HOME/.rvm/scripts/rvm" ]] ; then
+  source "$HOME/.rvm/scripts/rvm"
+elif [[ -s "/usr/local/rvm/scripts/rvm" ]] ; then
+  source "/usr/local/rvm/scripts/rvm"
+else
+  printf "ERROR: An RVM installation was not found.\n"
+fi
 echo '* Updating bundle'
 /home/aidancornelius/projects/bridging-the-gap/bin/bundle
 echo '* Restarting Ruby server - Puma'
