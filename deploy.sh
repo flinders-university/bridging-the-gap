5afd65c22ff790801bcd1ca94e8905c78275f047
Aidan Cornelius-Bell <aidan.cornelius-bell@flinders.edu.au>
2016-09-12 15:48:47 +0930

    fixed deploy

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,6 @@ for KILLPID in `ps ax | grep 'foreman' | awk ' { print $1;}'`; do
   kill -9 $KILLPID > /dev/null;
 done
 echo '** Restarting server...'
-`/usr/local/rvm/gems/ruby-2.3.1/bin/foreman start -f /home/aidancornelius/projects/bridging-the-gap/Procfile &`
+/usr/local/rvm/gems/ruby-2.3.1/bin/foreman start -f "/home/aidancornelius/projects/bridging-the-gap/Procfile" &
 echo
 echo '* Deploy Complete'
