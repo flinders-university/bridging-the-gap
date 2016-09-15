299e932a6bba23ccc31f0293062e6ce23f207126
Aidan Cornelius-Bell <aidan.cornelius-bell@flinders.edu.au>
2016-09-15 15:05:00 +0930

    Added show info to students pool

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160907045831) do
+ActiveRecord::Schema.define(version: 20160915052821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -118,6 +118,7 @@ ActiveRecord::Schema.define(version: 20160907045831) do
     t.boolean  "active"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.boolean  "show_info"
   end
 
   create_table "meetings", force: :cascade do |t|
