66d7ce747e7455865bdb87dbcf87330fa2b4d1a5
Aidan Cornelius-Bell <aidan.cornelius-bell@flinders.edu.au>
2016-12-09 10:17:49 +1030

    Kill spring BEFORE migrations

--- deploy.sh
+++ deploy.sh
@@ -13,24 +13,26 @@ echo '* Updating bundle'
 /home/aidancornelius/projects/bridging-the-gap/bin/bundle
 echo '* Repairing all RVM links'
 rvm repair all
-echo '* Repairing RVM in project folder'
-cd /home/aidancornelius/projects/bridging-the-gap/bin/ && rvm repair all
-echo '* DB Migrations '
-cd /home/aidancornelius/projects/bridging-the-gap && /home/aidancornelius/projects/bridging-the-gap/bin/rake db:migrate
-echo '* Restarting Ruby server - Puma'
-echo '** Killing existing server...'
+echo '* Killing existing web workers'
 for KILLPID in `ps ax | grep 'puma' | awk ' { print $1;}'`; do
   kill -9 $KILLPID > /dev/null;
 done
-echo '** Killing control protocol...'
+echo '** Killing worker control protocol'
 for KILLPID in `ps ax | grep 'foreman' | awk ' { print $1;}'`; do
   kill -9 $KILLPID > /dev/null;
 done
-echo '** Killing spring...'
+echo '** Killing rails spring'
 for KILLPID in `ps ax | grep 'spring' | awk ' { print $1;}'`; do
   kill -9 $KILLPID > /dev/null;
 done
-echo '** Restarting server...'
+echo '** If the spring does not terminate, migrations can not occur'
+echo '** learn more about spring termination in Rails documentation.'
+echo ' '
+echo '* Repairing RVM in project folder'
+cd /home/aidancornelius/projects/bridging-the-gap/bin/ && rvm repair all
+echo '* Running Database Migrations'
+cd /home/aidancornelius/projects/bridging-the-gap && /home/aidancornelius/projects/bridging-the-gap/bin/rake db:migrate
+echo '* Restarting web workers'
 puma --dir /home/aidancornelius/projects/bridging-the-gap/ -C /home/aidancornelius/projects/bridging-the-gap/config/puma.rb &
 echo
 echo '* Deploy Complete'
