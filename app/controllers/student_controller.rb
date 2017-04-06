f6bf23c5aa056978523c72c4765921a4fb09232e
Aidan Cornelius-Bell <aidan@teachersolutions.com.au>
2017-04-06 10:48:14 +0930

    Setup scaffold

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -2,5 +2,15 @@ class StudentController < ApplicationController
   def index
   end
   def survey
+    @isurvey = ISurvey.where(id: params[:id])
+    if @isurvey.count >= 1
+      @isurvey = @isurvey.first
+      @iquestions = @isurvey.i_questions
+    else
+      redirect_to "/student", alert: "Sorry, that survey isn't available yet."
+    end
+  end
+  def save
+    redirect_to "/student", notice: "Thank you for participating."
   end
 end
