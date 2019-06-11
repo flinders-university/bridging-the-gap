7e66d67d84ca8f1ea874123cce0112a0e4196a45
Aidan Cornelius-Bell <aidan@teachersolutions.com.au>
2019-06-11 15:19:15 +0930

    added scope

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -247,7 +247,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :microsoft_graph, ENV['AZURE_APPLICATION_CLIENT_ID'], ENV['AZURE_APPLICATION_CLIENT_SECRET']
+  config.omniauth :microsoft_graph, ENV['AZURE_APPLICATION_CLIENT_ID'], ENV['AZURE_APPLICATION_CLIENT_SECRET'], scope: User.Read
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
