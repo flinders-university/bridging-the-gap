113f35485addc24eb3068056cad5521363e9a1d2
Aidan Cornelius-Bell <aidan@teachersolutions.com.au>
2019-06-11 15:01:58 +0930

    Changed log target

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,8 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  # config.log_level = :debug
+  config.log_level = :info
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
@@ -87,7 +88,6 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
-
   logger           = ActiveSupport::Logger.new(STDOUT)
   #logger.formatter = config.log_formatter
   #config.logger = ActiveSupport::TaggedLogging.new(logger)
