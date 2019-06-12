d398273646146f4192f10445c400ee769895b93b
Aidan Cornelius-Bell <aidan@teachersolutions.com.au>
2019-06-12 10:44:01 +0930

    Fixed an overflow bug

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,7 +7,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, kind: "Microsoft Office") if is_navigational_format?
     else
-      session["devise.microsoft_graph"] = request.env["omniauth.auth"]
+      #session["devise.microsoft_graph"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
   end
