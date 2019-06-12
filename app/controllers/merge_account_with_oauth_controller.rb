8d8cc9f83e6bf55ce4e6aa62a9fd2bb3704dceba
Aidan Cornelius-Bell <aidan@teachersolutions.com.au>
2019-06-12 12:17:13 +0930

    Flashing

--- app/controllers/merge_account_with_oauth_controller.rb
+++ app/controllers/merge_account_with_oauth_controller.rb
@@ -1,13 +1,13 @@
 class MergeAccountWithOauthController < ApplicationController
   def notice
-    Rails.logger.fatal("Reached merge account with: #{params.inspect}")
-    if !params[:data]
+    Rails.logger.fatal("Reached merge account with: #{flash.inspect}")
+    if !flash[:data]
       redirect_to "/users/sign_in", notice: "Data missing. Please log in using your account manually."
     else
-      data = params[:data]
-      user = params[:user]
-      new_provider = params[:new_provider]
-      new_uid = params[:new_uid]
+      data = flash[:data]
+      user = flash[:user]
+      new_provider = flash[:new_provider]
+      new_uid = flash[:new_uid]
     end
   end
 end
