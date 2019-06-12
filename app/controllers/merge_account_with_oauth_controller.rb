2374993e750be3f7e65aa82ffe4e11855a8c0517
Aidan Cornelius-Bell <aidan@teachersolutions.com.au>
2019-06-12 12:00:39 +0930

    MMM

--- app/controllers/merge_account_with_oauth_controller.rb
+++ app/controllers/merge_account_with_oauth_controller.rb
@@ -1,7 +1,12 @@
 class MergeAccountWithOauthController < ApplicationController
   def notice
-    if !data
+    if !params[:data]
       redirect_to "/users/sign_in", notice: "Data AWOL"
+    else
+      data = params[:data]
+      user = params[:user]
+      new_provider = params[:new_provider]
+      new_uid = params[:new_uid]
     end
   end
 end
