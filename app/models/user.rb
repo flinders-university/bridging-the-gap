d0dd9dcf5af52eec3f3679cf3babbfbc9acc3732
Aidan Cornelius-Bell <aidan@teachersolutions.com.au>
2019-06-12 11:23:58 +0930

    fixnum issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -65,7 +65,7 @@ class User < ApplicationRecord
         user.password = Devise.friendly_token[0, 20]
         user.firstname = auth.info.first_name   # assuming the user model has a name
         user.lastname = auth.info.last_name
-        user.group = 3
+        user.group_id = 3
         # user.image = auth.info.image # assuming the user model has an image
         # If you are using confirmable and the provider(s) you use validate emails,
         # uncomment the line below to skip the confirmation emails.
