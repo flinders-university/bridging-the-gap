bc5eaef12f7a1f0e3f452b749da1132fe1832844
Aidan Cornelius-Bell <aidan@teachersolutions.com.au>
2019-06-12 10:59:24 +0930

    user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -56,6 +56,7 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      Rails.logger.fatal("Request response from MS AD required info: SN: #{auth.info.surname} FN: #{auth.info.surname} EM: #{auth.info.email}")
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.firstname = auth.info.name.first   # assuming the user model has a name
