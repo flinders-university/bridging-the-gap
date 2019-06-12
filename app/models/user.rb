70c09e3b37308dd0d1eb1dfd6eb527ae140c67ec
Aidan Cornelius-Bell <aidan@teachersolutions.com.au>
2019-06-12 10:36:47 +0930

    Fixed a classing issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,7 +54,7 @@ class User < ApplicationRecord
     UserMailer.welcome_email(self).deliver_now
   end
 
-  def from_omniauth(auth)
+  def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
