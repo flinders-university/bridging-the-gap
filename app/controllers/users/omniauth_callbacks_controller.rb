e3785374b35d0cc2ba366d52651f95b4611f7451
Aidan Cornelius-Bell <aidan@teachersolutions.com.au>
2019-06-12 14:49:08 +0930

    Fixed?

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,10 +2,16 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def microsoft_graph
     auth = request.env["omniauth.auth"]
 
-    if @u = User.find_by_email(auth.info.email) && @u.provider == "" 
-      #this catches accounts that already exist
-      redirect_to "/merge_account_with_oauth/notice", flash: { data: auth.info, new_provider: auth.provider, new_uid: auth.uid, user: @u }
-      false
+    if @u = User.find_by_email(auth.info.email)
+      if @u.provider != ""
+        sign_in(@u)
+        if @u.persisted?
+          sign_in_and_redirect @u, event: :authentication #this will throw if @user is not activated
+          set_flash_message(:notice, :success, kind: "Microsoft Office") if is_navigational_format?
+        else
+          #this catches accounts that already exist
+          redirect_to "/merge_account_with_oauth/notice", flash: { data: auth.info, new_provider: auth.provider, new_uid: auth.uid, user: @u }
+      end
     else
       # new accounts
       @user = User.from_omniauth(request.env["omniauth.auth"])
