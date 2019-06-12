a3da39aa721c87ef6d82b417c61baab9d396b0a1
Aidan Cornelius-Bell <aidan@teachersolutions.com.au>
2019-06-12 10:34:39 +0930

    Updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,7 +54,7 @@ class User < ApplicationRecord
     UserMailer.welcome_email(self).deliver_now
   end
 
-  def self.from_omniauth(auth)
+  def from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
@@ -63,7 +63,7 @@ class User < ApplicationRecord
       # user.image = auth.info.image # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails,
       # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
+      user.skip_confirmation!
     end
   end
 
